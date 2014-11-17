20cb0af301ddaea8d8afe45332e4f95bb2b36434
shixk <xiaokun.shi@stark-corp.com>
2014-11-17 10:43:24 +0800

    移除壁纸应用后台程序

--- mobileSrv/PictureInfo/urls.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'shixk'
-
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('PictureInfo.views',
-    url(r'^get_pic$', 'get_pic'),
-    url(r'^get_basetime$', 'get_basetime'),
-)
